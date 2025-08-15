b1082f212b10febe6f61cc4ee4e13cb9825e46a3
Ahmad-Reza-Alfayiet <ahmadrezaalfayiet@gmail.com>
2025-08-15 11:11:50 +0800

    Menambahkan konfigurasi Firebase dan emulator, serta mendefinisikan entitas pengguna.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
-import 'package:cipta_vera_mandiri_digital/app/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'firebase_options.dart';
+import 'app/routes/app_routes.dart';
 import 'app/routes/app_pages.dart';
 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // Connect to Firebase Auth Emulator (only for local testing)
+  // Remove this line when going to production
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
 
-
-void main() {
-   runApp(
+  runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: Routes.LOGIN,   // Mulai dari halaman login
-      getPages: AppPages.routes,    // Daftar route yang sudah kamu definisikan
+      initialRoute: Routes.LOGIN,
+      getPages: AppPages.routes,
     ),
   );
 }
